8818acd00b55467e4fa6948693947b49d27efba4
Alex Kochurov <alex.kochurov@gmail.com>
2024-03-13 23:50:59 +0300

    Add the pagination functionality

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,11 +1,16 @@
 class CustomersController < ApplicationController
+  PAGE_SIZE = 10
+
   def index
+    @page = params[:page].to_i
     @customers =
       if params[:keywords].present?
         @keywords = params[:keywords]
         search_term = CustomerSearchTerm.new(@keywords)
         Customer.where(search_term.where_clause, search_term.where_args)
                 .order(search_term.order)
+                .offset(PAGE_SIZE * @page)
+                .limit(PAGE_SIZE)
       else
         []
       end
